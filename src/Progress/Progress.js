d7ec083aea3fe9e9d6d753ebf037d09068c8e443
davidelden <deelden+github@gmail.com>
2019-10-02 11:33:08 -0500

    clean up indentations

--- src/Progress/Progress.js
+++ src/Progress/Progress.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 const Progress = props => {
-	const { answersLength, totalQuestions } = props,
-				percentComplete = Math.floor((answersLength/totalQuestions)*100);
+  const { answersLength, totalQuestions } = props,
+        percentComplete = Math.floor((answersLength/totalQuestions)*100);
 
-	let barStyle = {};
+  let barStyle = {};
 
-	if(isNaN(percentComplete)) {
-		barStyle = { width: "0%" }
-	} else {
-		barStyle = { width: percentComplete.toString() + "%"}
-	}
+  if(isNaN(percentComplete)) {
+    barStyle = { width: "0%" }
+  } else {
+    barStyle = { width: percentComplete.toString() + "%"}
+  }
 
-	return (
-		<React.Fragment>
-			<h5>Progress:</h5>
+  return (
+    <React.Fragment>
+      <h5>Progress:</h5>
       <div className="progress mb-4">
         <div className="progress-bar" style={barStyle} />
       </div>
-		</React.Fragment>
-	)
+    </React.Fragment>
+  )
 }
 
 export default Progress;
--- src/hoc/Wrapper.js
+++ src/hoc/Wrapper.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const Wrapper = (WrappedComponent) => {
-	return props => (
-		<div className="container">
+  return props => (
+    <div className="container">
       <div className='row'>
         <div className='col-md-6 pt-4'>
-        	<WrappedComponent {...props} />
+          <WrappedComponent {...props} />
         </div>
       </div>
     </div>
-	)
+  )
 }
 
 export default Wrapper;
